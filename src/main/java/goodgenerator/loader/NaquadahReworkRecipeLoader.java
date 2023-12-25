05b70fe8d4aeda025904068b7d950cf3f4d891d8
Martin Robertz <dream-master@gmx.net>
2023-12-25 12:18:59 +0100

    Update NaquadahReworkRecipeLoader.java

--- src/main/java/goodgenerator/loader/NaquadahReworkRecipeLoader.java
+++ src/main/java/goodgenerator/loader/NaquadahReworkRecipeLoader.java
@@ -1126,10 +1126,8 @@ public class NaquadahReworkRecipeLoader {
                 if (GT_Utility.isStackValid(input)) {
                     int[] oreDict = OreDictionary.getOreIDs(input);
                     for (int oreDictID : oreDict) {
-                        // spotless:off
                         if (OreDictionary.getOreName(oreDictID).startsWith("dustImpureNaq")
-                            || OreDictionary.getOreName(oreDictID).startsWith("dustPureNaq")) {
-                            // spotless:on
+                                || OreDictionary.getOreName(oreDictID).startsWith("dustPureNaq")) {
                             GT_Recipe tRecipe = recipe.copy();
                             boolean modified = false;
                             for (int i = 0; i < tRecipe.mOutputs.length; i++) {
